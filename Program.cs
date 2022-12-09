a4c91fa3b408cb517ebaa7007b404e1ca3665474
alex <derdiii@mail.ru>
2022-12-09 22:10:52 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int A = ReadInt("Введите число A ");
+int B = ReadInt("Введите число B ");
+
+int res = A;
+for(int i = 1; i < B; i++)
+{
+    res *= A;
+}
+Console.WriteLine(res);
+
+
+
+
+
+
+
